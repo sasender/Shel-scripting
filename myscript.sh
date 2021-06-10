5cf2402d1522d0c8c0c905ae2bee3ec8f7ac27b5
Sasendra <Sasendra@sasendra.com>
2021-06-10 23:21:29 +0530

    sample

---
+++ myscript.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+youare=thief
+echo "hi $youare"
---
+++ sample.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# this line intented to be usec genral descrption
+#and anything that it does
+clear  #claers the screen
+MYUSERNAME="Terry" #the username for this application
+MYUSERNAME="don"  #new username added
+
+echo "we are using the default user called: $MYUSERNAME" #display to the console
+
+DATETIMESTAMP=`date`
+
+echo "this is when the script was run: $DATETIMESTAMP" #this is the time stamp of run
