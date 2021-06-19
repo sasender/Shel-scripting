ea33bcefe1ed6864496aa358657ef896caf53ba6
Sasendra <Sasendra@sasendra.com>
2021-06-19 15:12:00 +0530

    changes

--- cmdline.sh
+++ cmdline.sh
@@ -2,4 +2,4 @@
 #demo of cmd line varibles passing the through script
 username=$1
 password=$2
-echo "the following user name is $username ans password is $password"
+echo "the following user name is $username and password is $password"
