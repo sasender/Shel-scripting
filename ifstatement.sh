fce01f9eaa3a4c6f5ffe314b60ff09f9cb697c65
Sasendra <Sasendra@sasendra.com>
2021-06-21 23:14:57 +0530

    if state

---
+++ ifstatement.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#test for existence of indicated file name
+FILENAME=$1
+echo "testing for the existense of a file called $FILENAME"
+
+if [ ! -a $FILENAME ]
+  then
+    echo "file $filename does indeed exist!"
+fi 
+#fi is the if statement    
+# ! it shows the existing file or not existing file 
+# with out ! thise value iy shows same files either exist or non exist files 
