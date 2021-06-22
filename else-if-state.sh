b7a2979ebe8fd490ca8c14206d9b8f8c23dfd654
Sasendra <Sasendra@sasendra.com>
2021-06-22 23:16:06 +0530

    something

--- else-if-state.sh
+++ else-if-state.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-AGE=$1
-if [ $AGE  15 ]; then
-    echo "You are a girl child."
-elif [ $AGE  22 ]; then
-    echo "you are a teanage girl."
-elif [ $AGE  35 ]; then
-    echo "you are a gorgeous adult women"
-else 
-    echo "you are an elder women"
 
-fi               
+AGE=$1
+
+if [ $AGE -lt 13 ]; then
+	echo "You are a kid."
+elif [ $AGE -lt 20 ]; then
+	echo "You are a teenager."
+elif [ $AGE -lt 65 ]; then
+	echo "You are an adult."
+else
+	echo "You are an elder."
+fi
