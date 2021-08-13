79b07471d6704c03663c4713ab91f2535ad28dbb
Sasendra <Sasendra@sasendra.com>
2021-08-13 18:35:33 +0530

    execution

---
+++ execution-oparator.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# this eis the execution oprator example
+echo "Enter a number between 1 and 5:"
+read value
+if [ " $value" -eq "1" ] || [ "$value" -eq "3" ] || [ "$value" -eq "5" ]
+  echo "yup you enterd base numbers of $value"
+else
+ echo "yup entered a value od $value"
+fi
