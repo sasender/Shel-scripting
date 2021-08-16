2f800a568817af0e255673151fdc88bb44b32607
Sasendra <Sasendra@sasendra.com>
2021-08-16 17:19:03 +0530

    mistakes

--- execution-oparator.sh
+++ execution-oparator.sh
@@ -2,8 +2,8 @@
 # this eis the execution oprator example
 echo "Enter a number between 1 and 5:"
 read value
-if [ " $value" -eq "1" ] || [ "$value" -eq "3" ] || [ "$value" -eq "5" ]
+if [ " $value" -eq "1" ] || [ "$value" -eq "3" ] || [ "$value" -eq "5" ]; then
   echo "yup you enterd base numbers of $value"
- else
+else
  echo "yup entered a value od $value"
 fi
