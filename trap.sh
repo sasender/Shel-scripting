4eadd48ec155e08947259c718d68dc7da2d523e9
Sasendra <Sasendra@sasendra.com>
2021-08-16 18:48:10 +0530

    trap

---
+++ trap.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#example of trapping events and limiting the sell stoping
+clear
+
+while [ "$choice" !="Q" ] && [ "$choice" ! ="q" ]; do
+ echo "main menu"
+ echo "========="
+ echo "1) choice one"
+ echo "2) choice two"
+ echo "3) choice three"
+ echo "Q) Quit/Exit"
+ echo ""
+ read choice
+
+
+ clear
+done
+ 
