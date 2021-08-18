2613333f7a2c06190a20dcd43cbfd1192e705cf5
Sasendra <Sasendra@sasendra.com>
2021-08-18 17:31:51 +0530

    -s

--- useradding.sh
+++ useradding.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -xe
 read -p "please enter the user name:" USER_NAME
-read -p "please enter the password:" USER_PASS
+read -s -p "please enter the password:" USER_PASS
 echo ${USER_NAME}
 echo ${USER_PASS}
+#-s id the encrpted our passwords 
+#-p is the prompted our read format
